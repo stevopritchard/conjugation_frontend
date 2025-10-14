0cbab9bfa812c97cc042f08c8016193b1ac0ddf6
stevopritchard <stephen.a.pritchard@gmail.com>
2025-10-14 20:15:50 +0100

    Import context provider to App and wrap children

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import {Reference} from './containers/Reference';
-import {Practise} from './containers/Practise';
-import {SignIn} from './components/SignIn';
-import {Register} from './components/Register';
+import { Reference } from './containers/Reference';
+import { Practise } from './containers/Practise';
+import { SignIn } from './components/SignIn';
+import { Register } from './components/Register';
+import AuthContextProvider from './store/auth-context';
 import './App.css';
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      route: "signin",
+      route: 'signin',
       signedIn: false,
       mode: 'reference',
       user: {
@@ -18,24 +19,23 @@ class App extends React.Component {
         name: '',
         email: '',
         favourites: [],
-        joined: ''
-      }
+        joined: '',
+      },
     };
   }
 
-  onRouteChange = ( route ) => {
-      
-    this.setState({route: route})
-    if(route === 'signin' || route === 'register') {
-      this.setState({signedIn: false})
-    } else if(route === 'home'){
-      this.setState({signedIn: true})
+  onRouteChange = (route) => {
+    this.setState({ route: route });
+    if (route === 'signin' || route === 'register') {
+      this.setState({ signedIn: false });
+    } else if (route === 'home') {
+      this.setState({ signedIn: true });
     }
-  }
+  };
 
   onModeChange = (mode) => {
-    this.setState({mode: mode})
-  }
+    this.setState({ mode: mode });
+  };
 
   loadUser = (user) => {
     this.setState({
@@ -44,52 +44,42 @@ class App extends React.Component {
         name: user.name,
         email: user.email,
         favourites: user.favourites,
-        joined: user.joined
-      }
-    })
-  }
-  
+        joined: user.joined,
+      },
+    });
+  };
 
   render() {
-    const { 
-      route,
-      mode
-    } = this.state;
+    const { route, mode } = this.state;
     return (
       <div className="App">
-        <Header 
-          isSignedIn={this.state.signedIn} 
-          routeChange={this.onRouteChange}
-          modeChange={this.onModeChange}
-        />
-        { route === 'home'
-          ?
-          ( mode === 'reference'
-            ?
-            <Reference
-              id={this.state.user.id}
-              favourites={this.state.user.favourites}
-            />
-            :
-            <Practise />
-          )
-          : 
-          ( route ==='signin' 
-            ?
-            <SignIn 
+        <AuthContextProvider>
+          <Header
+            isSignedIn={this.state.signedIn}
+            routeChange={this.onRouteChange}
+            modeChange={this.onModeChange}
+          />
+          {route === 'home' ? (
+            mode === 'reference' ? (
+              <Reference
+                id={this.state.user.id}
+                favourites={this.state.user.favourites}
+              />
+            ) : (
+              <Practise />
+            )
+          ) : route === 'signin' ? (
+            <SignIn routeChange={this.onRouteChange} loadUser={this.loadUser} />
+          ) : (
+            <Register
               routeChange={this.onRouteChange}
               loadUser={this.loadUser}
             />
-            :
-            <Register 
-              routeChange={this.onRouteChange}
-              loadUser={this.loadUser}
-            />
-          )
-        }
+          )}
+        </AuthContextProvider>
       </div>
-    )
+    );
   }
-};
+}
 
 export default App;
